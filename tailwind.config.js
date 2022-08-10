fdc416240cafae1ad3c63b025440ed0fcae457d5
Jasen Chawda <jasen.chawda@navos.co.uk>
2022-08-10 14:22:01 +0100

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,25 @@ module.exports = {
         // add custom colors here
         // customColor: "#HEXCODE"
       },
+      keyframes: {
+        wiggle: {
+          "0%": { transform: "translateX(1%)" },
+          "10%": { transform: "translateX(-1%)" },
+          "20%": { transform: "translateX(1%)" },
+          "30%": { transform: "translateX(-1%)" },
+          "40%": { transform: "translateX(1%)" },
+          "50%": { transform: "translateX(-1%)" },
+          "60%": { transform: "translateX(1%)" },
+          "70%": { transform: "translateX(-1%)" },
+          "80%": { transform: "translateX(1%)" },
+          "90%": { transform: "translateX(-1%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      animation: {
+        // wiggle: "wiggle 2s infinite",
+        wiggle: "wiggle 0.5s linear 1",
+      },
     },
   },
   plugins: [],
